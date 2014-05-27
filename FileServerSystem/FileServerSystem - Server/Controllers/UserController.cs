5e44be0a2c883727d2192fe15cbc25bee6b4d2e6
karol1990 <pawlowskikarol1990@gmail.com>
2014-05-28 00:04:56 +0200

    Log4net + LinqToSql + UserController changes

--- FileServerSystem/FileServerSystem - Server/Controllers/UserController.cs
+++ FileServerSystem/FileServerSystem - Server/Controllers/UserController.cs
@@ -1,54 +1,80 @@
 ﻿using FileServerSystem___Server.Contracts;
 using System;
 using System.Collections.Generic;
+using System.Data.Linq;
+using System.Diagnostics.Contracts;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace FileServerSystem___Server.Controllers
 {
     public class UserController : Controller, IUserController
     {
-        private IDictionary<string, string> users;
-        private IDictionary<string, Guid> tokens;
+        private Table<USER> _users;
+        private Table<TOKEN> _tokens;
+        private DataDataContext _dataContext;        
 
         public UserController()
         {
-            users = new Dictionary<string, string>();
-            tokens = new Dictionary<string, Guid>();
+            _dataContext = new DataDataContext();
+
+            _users = _dataContext.GetTable<USER>();
+            _tokens = _dataContext.GetTable<TOKEN>();            
         }
 
         public string RegisterNewUser(string userName, string password)
         {
-            users.Add(userName, password);
+            Contract.Requires(userName != null && password != null & userName != string.Empty & password != string.Empty);           
 
+            Table<USER> users = _dataContext.GetTable<USER>();
+            users.InsertOnSubmit(new USER(userName, password));
+
+            Contract.Ensures(Contract.Result<string>() != string.Empty);
             return "User has been added";
         }
 
         public string UserLogin(string userName, string password)
         {
-            if (users.ContainsKey(userName))
+            Contract.Requires(userName != null && password != null & userName != string.Empty & password != string.Empty);
+            
+            if ((from user in _users where user.Login == userName select user).FirstOrDefault() != null)
             {
-                tokens.Add(userName, Guid.NewGuid());
+                _tokens.InsertOnSubmit(new TOKEN(userName, Guid.NewGuid().ToString()));
             }
             else
             {
                 return "User is not registered";
             }
 
-            return tokens[userName].ToString();
+            Contract.Ensures(Contract.Result<string>() != string.Empty);
+
+            return (from token in _tokens where token.USER.Login == userName select token).FirstOrDefault().ToString();
         }
 
         public string UserLogOff(string userName)
         {
-            if (users.ContainsKey(userName))
+            Contract.Requires(userName != null & userName != string.Empty);
+
+            if ((from user in _users where user.Login == userName select user).FirstOrDefault() != null)
             {
-                tokens.Remove(userName);
+                _tokens.DeleteOnSubmit((from token in _tokens where token.USER.Login == userName select token).FirstOrDefault());
             }
             else
             {
                 return "User is not registered";
             }
 
+            Contract.Ensures(Contract.Result<string>() != string.Empty);
+
             return "User has been logged off";
+        }
+
+        [ContractInvariantMethod]
+        protected void ObjectInvariant () 
+        {
+            Contract.Invariant(_users != null);
+            Contract.Invariant(_tokens != null);
+            Contract.Invariant(_dataContext != null);
         }
     }
 }
